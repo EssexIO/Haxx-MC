dd5135933243360b8335576db9cfc73f82c7807b
EssexIO <essexwebsec@gmail.com>
2014-05-01 23:55:59 -0700

    The first commit. I love saying that. :3

---
+++ Haxx/src/me/EssexIO/Main.java
@@ -0,0 +1,15 @@
+package me.EssexIO;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	// Setting things up.
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
